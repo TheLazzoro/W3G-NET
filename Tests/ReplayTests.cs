5ca864fdb280ee314a11cea890d8eab84da7ffce
TheLazzoro <lasse.d@live.dk>
2024-09-28 22:10:11 +0200

    Tests

---
+++ Tests/ReplayTests.cs
@@ -0,0 +1,35 @@
+using W3GNET;
+
+[assembly: Parallelize(Workers = 8, Scope = ExecutionScope.ClassLevel)]
+namespace Tests
+{
+    [TestClass]
+    public class ReplayTests
+    {
+        static string replaysFolder = Path.Combine(Directory.GetCurrentDirectory(), "Replays");
+
+        public static IEnumerable<object[]> GetReplaysFromTestFolder
+        {
+            get
+            {
+                string[] replays = Directory.GetFileSystemEntries("Replays\\", "*.w3g", SearchOption.AllDirectories);
+                var objects = new List<object[]>();
+                for (int i = 0; i < replays.Length; i++)
+                {
+                    yield return new[] { Path.Combine(Directory.GetCurrentDirectory(), replays[i]) };
+                }
+            }
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetReplaysFromTestFolder), typeof(ReplayTests), DynamicDataSourceType.Property)]
+        public async Task ReplayTestsAll(string replayPath)
+        {
+            using (var s = new FileStream(replayPath, FileMode.Open))
+            {
+                W3GReplay replay = new W3GReplay();
+                await replay.Parse(s);
+            }
+        }
+    }
+}
